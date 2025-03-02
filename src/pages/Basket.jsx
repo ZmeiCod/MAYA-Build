a2c21516e13a31aa92354138a0961bf66665a26b
NoNameCoder <johnthetheologian@mail.ru>
2025-03-02 13:04:12 +0300

    Update

--- src/pages/Basket.jsx
+++ src/pages/Basket.jsx
@@ -64,7 +64,7 @@ export default function Basket() {
   const productModifiers = {};
 
   const params = {
-    secret: frontpad, // ключ api
+    secret: frontpad,
     street: address,
     name: name,
     phone: phone,

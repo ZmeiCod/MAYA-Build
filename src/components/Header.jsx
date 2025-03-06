9aab8b52396c52089bba41840a768fa53a2973a3
NoNameCoder <johnthetheologian@mail.ru>
2025-03-06 11:48:18 +0300

    add time

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import logo from "../assets/img/logo.png";
 // import Search from "./Search";
 import honeycombs from "../assets/img/honeycombs.svg";
-import basket from '../assets/ui/basket.svg'
+import basket from "../assets/ui/basket.svg";
 import { useSelector } from "react-redux";
 
 function Header() {
@@ -22,14 +22,15 @@ function Header() {
           </div>
         </Link>
         {/* <Search /> */}
-        <div className='header__phone'>
+        <div className="header__phone">
           <a href="tel:+79784441414"> + 7 (978) 444 14 14 </a>
+          <p>Пн-чт 10.00-23-00 <br /> Пт-вс 10.00-00.00</p>
         </div>
         <div className="header__cart">
           <Link to="/basket" className="button button--cart">
             <span>{totalPrice} ₽</span>
             <div className="button__delimiter"></div>
-            <img className='header__button--basket' src={basket} alt="basket" />
+            <img className="header__button--basket" src={basket} alt="basket" />
             <span>{totalCount}</span>
           </Link>
         </div>
